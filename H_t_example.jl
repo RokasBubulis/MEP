026109596b22dcc54e200bc2b61def797b3aeef1
Rokas Bubulis <rok.bubulis@gmail.com>
2025-09-10 15:30:13 +0200

    removed initial file

--- H_t_example.jl
+++
@@ -1,9 +0,0 @@
-using QuantumOptics
-basis = SpinBasis(1//2)
-ψ₀ = spindown(basis)
-const sx = sigmax(basis)
-function H_pump(t, psi)
-  return sin(t)*sx
-end
-tspan = [0:0.1:10;]
-tout, ψₜ = timeevolution.schroedinger_dynamic(tspan, ψ₀, H_pump)
