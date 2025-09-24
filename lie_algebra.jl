3e0f2ac61f2bdcc01bfc5ff8ba08b0e0091d21b7
Rokas Bubulis <rok.bubulis@gmail.com>
2025-09-24 16:52:12 +0200

    Local and global comparison

--- lie_algebra.jl
+++ lie_algebra.jl
@@ -1,3 +1,25 @@
+# Default matrix exponentiation approach
+function standard_expectation_value(
+    observable::SparseMatrixCSC{ComplexF64, Int}, 
+    input_matrix::SparseMatrixCSC{Float64, Int}, 
+    theta::Vector{Float64}, 
+    generators::Vector{SparseMatrixCSC{ComplexF64, Int}})
+
+    tmpH = similar(Matrix(generators[1]))
+    mul!(tmpH, Matrix(generators[1]), theta[1])
+    BLAS.axpy!(theta[2], Matrix(generators[2]), tmpH) 
+    lmul!(-im, tmpH)                           
+    unitary = exp(tmpH) 
+    tmp1 = similar(unitary)
+    tmp2 = similar(unitary)
+    tmp3 = similar(unitary)
+    mul!(tmp1, observable, unitary)
+    mul!(tmp2, tmp1, input_matrix)
+    mul!(tmp3, tmp2, unitary')                  
+    return tr(tmp3)
+end 
+
+# Lie-algebraic approach
 br(A, B) = A * B - B * A
 
 function is_independent(matrix_list::Vector{<:SparseMatrixCSC{ComplexF64, Int}}, candidate_matrix::SparseMatrixCSC{ComplexF64, Int}; tol_rel=1e-12,tol_abs=1e-14)
--- main.jl
+++ main.jl
@@ -1,27 +1,6 @@
 using BenchmarkTools
 include("generators.jl")
 include("lie_algebra.jl")
-include("decomposition.jl")
-
-function standard_expectation_value(
-    observable::SparseMatrixCSC{ComplexF64, Int}, 
-    input_matrix::SparseMatrixCSC{Float64, Int}, 
-    theta::Vector{Float64}, 
-    generators::Vector{SparseMatrixCSC{ComplexF64, Int}})
-
-    tmpH = similar(Matrix(generators[1]))
-    mul!(tmpH, Matrix(generators[1]), theta[1])
-    BLAS.axpy!(theta[2], Matrix(generators[2]), tmpH) 
-    lmul!(-im, tmpH)                           
-    unitary = exp(tmpH) 
-    tmp1 = similar(unitary)
-    tmp2 = similar(unitary)
-    tmp3 = similar(unitary)
-    mul!(tmp1, observable, unitary)
-    mul!(tmp2, tmp1, input_matrix)
-    mul!(tmp3, tmp2, unitary')                  
-    return tr(tmp3)
-end 
 
 commutation_depth = 10
 theta = [0.7, 0.6]
@@ -44,44 +23,3 @@ println("lie basis 2: $(length(lie_basis1))")
 
 println("<O> using standard approach: $(standard_expectation_value(observable, input_matrix, theta, generators))")
 println("<O> using gsim approach 2: $(gsim_expectation_value(observable, input_matrix, theta, lie_basis1, adjoint_map1))")
-
-# generators = nothing
-# observable = nothing 
-# input_matrix = nothing
-# lie_basis = nothing
-# adjoint_map = nothing
-# lie_basis1 = nothing
-# adjoint_map1 = nothing
-# for n_qubits in 2:7
-#     println("Number of qubits: $n_qubits")
-#     #observable = operator(XopRyd([1,2]), n_qubits) + operator(XopRyd([2]), n_qubits)
-#     global generators = construct_Ryd_generators(n_qubits)
-#     global observable = generators[1]
-#     global input_matrix = construct_input_matrix(n_qubits)
-
-#     global lie_basis = construct_lie_basis(generators, commutation_depth)
-#     global adjoint_map = construct_adjoint_representations(lie_basis, generators)
-#     println("Global GS. Lie basis dim: $(length(lie_basis))")
-#     global lie_basis1 = construct_lie_basis_fast(generators, commutation_depth)
-#     global adjoint_map1 = construct_adjoint_representations(lie_basis1, generators)
-#     println("Local GS. Lie basis dim: $(length(lie_basis1))")
-#     println("---")
-#     println("Benchmarks for Lie basis construction")
-#     println("global GS:")
-#     @btime construct_lie_basis(generators, commutation_depth)
-#     println("local GS:")
-#     @btime construct_lie_basis_fast(generators, commutation_depth)
-#     println("---")
-#     println("Observable expectation values")
-#     println("Standard <O>: $(standard_expectation_value(observable, input_matrix, theta, generators))")
-#     println("Global Lie <O>: $(gsim_expectation_value(observable, input_matrix, theta, lie_basis, adjoint_map))")
-#     println("Local Lie <O>: $(gsim_expectation_value(observable, input_matrix, theta, lie_basis1, adjoint_map1))")
-#     println("---")
-#     println("Simulation benchmarks")
-#     println("Standard:")
-#     @btime standard_expectation_value(observable, input_matrix, theta, generators)
-#     println("Lie:")
-#     @btime gsim_expectation_value(observable, input_matrix, theta, lie_basis1, adjoint_map1)
-#     println("---")
-#     println("---")
-# end
